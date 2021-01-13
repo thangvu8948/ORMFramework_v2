8d0d041c81641f26f3561d150d00638108003e5d
Đức Thắng <thangvu8948@gmail.com>
2021-01-13 21:45:21 +0700

    + db handler contract

---
+++ ORMFramework/Community/IDatabaseHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ORM.Community
+{
+    public interface IDatabaseHandler
+    {
+        IDbConnection CreateConnection();
+
+        void CloseConnection(IDbConnection connection);
+
+        IDbCommand CreateCommand(string commandText, CommandType commandType, IDbConnection connection);
+
+        IDataAdapter CreateAdapter(IDbCommand command);
+
+        IDbDataParameter CreateParameter(IDbCommand command);
+    }
+}
