8e784a0502d3093e5d27c333c486c472e0f86210
Đức Thắng <thangvu8948@gmail.com>
2021-01-13 21:52:29 +0700

    + use factory for db handler

---
+++ ORMFramework/Community/DatabaseHandlerFactory.cs
@@ -0,0 +1,47 @@
+﻿using _4_ORM.SupportedAccess;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ORM.Community
+{
+    public class DatabaseHandlerFactory
+    {
+        private ConnectionStringSettings connectionStringSettings;
+
+        public DatabaseHandlerFactory(string connectionStringName)
+        {
+            connectionStringSettings = ConfigurationManager.ConnectionStrings[connectionStringName];
+        }
+
+        public IDatabaseHandler CreateDatabase()
+        {
+            IDatabaseHandler database = null;
+
+            switch (connectionStringSettings.ProviderName.ToLower())
+            {
+                case "system.data.sqlclient":
+                    database = new MsSqlDataAccess(connectionStringSettings.ConnectionString);
+                    break;
+                case "mysql.data.mysqlclient":
+                    database = new MySqlDataAccess(connectionStringSettings.ConnectionString);
+                    break;
+                case "npgsql":
+                    database = new PostgreSqlDataAccess(connectionStringSettings.ConnectionString);
+                    break;
+                default:
+                    break;
+            }
+
+            return database;
+        }
+
+        public string GetProviderName()
+        {
+            return connectionStringSettings.ProviderName;
+        }
+    }
+}
