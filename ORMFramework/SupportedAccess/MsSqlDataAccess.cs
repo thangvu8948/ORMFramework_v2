639c4e91bba1abf501ef40620fc5c4b4c1382de5
Đức Thắng <thangvu8948@gmail.com>
2021-01-13 22:48:22 +0700

    + support MSSql

---
+++ ORMFramework/SupportedAccess/MsSqlDataAccess.cs
@@ -0,0 +1,54 @@
+﻿using _4_ORM.Community;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ORM.SupportedAccess
+{
+    class MsSqlDataAccess : IDatabaseHandler
+    {
+        private string ConnectionString { get; set; }
+
+        public MsSqlDataAccess(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+
+        public void CloseConnection(IDbConnection connection)
+        {
+            var sqlConnection = (SqlConnection)connection;
+            sqlConnection.Close();
+            sqlConnection.Dispose();
+        }
+
+        public IDbCommand CreateCommand(string commandText, CommandType commandType, IDbConnection connection)
+        {
+            return new SqlCommand
+            {
+                CommandText = commandText,
+                Connection = (SqlConnection)connection,
+                CommandType = commandType
+            };
+        }
+
+        public IDataAdapter CreateAdapter(IDbCommand command)
+        {
+            return new SqlDataAdapter((SqlCommand)command);
+        }
+
+        public IDbDataParameter CreateParameter(IDbCommand command)
+        {
+            SqlCommand SQLcommand = (SqlCommand)command;
+            return SQLcommand.CreateParameter();
+        }
+    }
+}
