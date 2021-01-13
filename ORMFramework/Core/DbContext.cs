670ec1b4341447add1cd07cd6428a1dd0d69d46e
SangDoan <doansang654@gmail.com>
2021-01-13 22:07:06 +0700

    # create DbContext DbSet ( the same entity framwork)

---
+++ ORMFramework/Core/DbContext.cs
@@ -0,0 +1,24 @@
+﻿using ORMFramework.Community;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMFramework.Core
+{
+    public abstract class DbContext
+    {
+        protected DBManager dBManager;
+        protected string name;
+        public DbContext(string _name)
+        {
+            name = _name;
+            dBManager = new DBManager(_name);
+        }
+        public DbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            return new DbSet<TEntity>(dBManager);
+        }
+    }
+}
---
+++ ORMFramework/Core/DbSet.cs
@@ -0,0 +1,21 @@
+﻿using ORMFramework.Community;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMFramework.Core
+{
+    public class DbSet<TEntity> where TEntity : class
+    {
+        string table = typeof(TEntity).Name;
+        private DBManager _dbManager;
+        string currentCommand = "";
+        public DbSet(DBManager dBManager)
+        {
+            _dbManager = dBManager;
+            //currentCommand = SqlQuery.selectSQL(table);
+        }
+    }
+}
