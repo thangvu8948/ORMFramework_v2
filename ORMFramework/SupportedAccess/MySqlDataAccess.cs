a966d9c18a44d79d845cabd6d9a6167048c33334
Đức Thắng <thangvu8948@gmail.com>
2021-01-13 22:48:38 +0700

    + support postgre

---
+++ ORMFramework/SupportedAccess/MySqlDataAccess.cs
@@ -0,0 +1,54 @@
+﻿using _4_ORM.Community;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ORM.SupportedAccess
+{
+    class MySqlDataAccess : IDatabaseHandler
+    {
+        private string ConnectionString { get; set; }
+
+        public MySqlDataAccess(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public void CloseConnection(IDbConnection connection)
+        {
+            var mysqlConnection = (MySqlConnection)connection;
+            mysqlConnection.Close();
+            mysqlConnection.Dispose();
+        }
+
+        public IDbCommand CreateCommand(string commandText, CommandType commandType, IDbConnection connection)
+        {
+            return new MySqlCommand
+            {
+                CommandText = commandText,
+                Connection = (MySqlConnection)connection,
+                CommandType = commandType
+            };
+        }
+
+        public IDataAdapter CreateAdapter(IDbCommand command)
+        {
+            return new MySqlDataAdapter((MySqlCommand)command);
+        }
+
+        public IDbDataParameter CreateParameter(IDbCommand command)
+        {
+            MySqlCommand SQLcommand = (MySqlCommand)command;
+            return SQLcommand.CreateParameter();
+        }
+    }
+}
