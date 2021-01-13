0ea9d08ae0b7890b33f00f395a73c6f3c0105fad
Đức Thắng <thangvu8948@gmail.com>
2021-01-13 21:52:12 +0700

    + add param manager

---
+++ ORMFramework/Community/DataParameterManager.cs
@@ -0,0 +1,117 @@
+﻿using MySql.Data.MySqlClient;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ORM.Community
+{
+    class DataParameterManager
+    {
+        public static IDbDataParameter CreateParameter(string providerName, string name, object value, DbType dbType, ParameterDirection direction = ParameterDirection.Input)
+        {
+            IDbDataParameter parameter = null;
+            switch (providerName.ToLower())
+            {
+                case "system.data.sqlclient":
+                    return CreateSqlParameter(name, value, dbType, direction);
+                case "system.data.mysqlclient":
+                    return CreateMySqlParameter(name, value, dbType, direction);
+                case "npgsql":
+                    return CreatePostgreSqlParameter(name, value, dbType, direction);
+                default:
+                    break;
+            }
+
+            return parameter;
+        }
+
+        public static IDbDataParameter CreateParameter(string providerName, string name, int size, object value, DbType dbType, ParameterDirection direction = ParameterDirection.Input)
+        {
+            IDbDataParameter parameter = null;
+            switch (providerName.ToLower())
+            {
+                case "system.data.sqlclient":
+                    return CreateSqlParameter(name, size, value, dbType, direction);
+                case "mysql.data.mysqlclient":
+                    return CreateMySqlParameter(name, size, value, dbType, direction);
+                case "npgsql":
+                    return CreatePostgreSqlParameter(name, value, dbType, direction);
+                default:
+                    break;
+            }
+
+            return parameter;
+        }
+
+        private static IDbDataParameter CreateSqlParameter(string name, object value, DbType dbType, ParameterDirection direction)
+        {
+            return new SqlParameter
+            {
+                DbType = dbType,
+                ParameterName = name,
+                Direction = direction,
+                Value = value
+            };
+        }
+
+        private static IDbDataParameter CreateSqlParameter(string name, int size, object value, DbType dbType, ParameterDirection direction)
+        {
+            return new SqlParameter
+            {
+                DbType = dbType,
+                Size = size,
+                ParameterName = name,
+                Direction = direction,
+                Value = value
+            };
+        }
+
+        private static IDbDataParameter CreateMySqlParameter(string name, object value, DbType dbType, ParameterDirection direction)
+        {
+            return new MySqlParameter
+            {
+                DbType = dbType,
+                ParameterName = name,
+                Direction = direction,
+                Value = value
+            };
+        }
+        private static IDbDataParameter CreateMySqlParameter(string name, int size, object value, DbType dbType, ParameterDirection direction)
+        {
+            return new MySqlParameter
+            {
+                DbType = dbType,
+                Size = size,
+                ParameterName = name,
+                Direction = direction,
+                Value = value
+            };
+        }
+        private static IDbDataParameter CreatePostgreSqlParameter(string name, object value, DbType dbType, ParameterDirection direction)
+        {
+            return new NpgsqlParameter
+            {
+                DbType = dbType,
+                ParameterName = name,
+                Direction = direction,
+                Value = value
+            };
+        }
+        private static IDbDataParameter CreatePostgreSqlParameter(string name, int size, object value, DbType dbType, ParameterDirection direction)
+        {
+            return new NpgsqlParameter
+            {
+                DbType = dbType,
+                Size = size,
+                ParameterName = name,
+                Direction = direction,
+                Value = value
+            };
+        }
+    }
+}
