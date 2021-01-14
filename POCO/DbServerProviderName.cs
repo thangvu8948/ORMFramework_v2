6c5ae536f55a2792b76f021fae9c25f35d9358c9
Tran Duy Quang <duyquang19981@gmail.com>
2021-01-14 20:07:04 +0700

    Facttory ReaderSchema

---
+++ POCO/DbServerProviderName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCO
+{
+    /// <summary>
+    /// This class represents DbServerProviderName class.
+    /// </summary>
+    public static class DbServerProviderName
+    {
+        public static string MsSql = "System.Data.SqlClient";
+        public static string MySql = "MySql.Data.MySqlClient";
+        public static string Postgres = "Npgsql";
+    }
+}
---
+++ POCO/Readers/SchemaReaderProvider.cs
@@ -0,0 +1,35 @@
+﻿using POCO.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCO.Readers
+{
+    public class SchemaReaderProvider
+    {
+        /// <summary>
+        /// This class represents a provier class to get right schema reader based on database type.
+        /// </summary>
+        public static SchemaReader GetReader(DbServerType dbServerType)
+        {
+            SchemaReader schemaReader = null;
+            switch (dbServerType)
+            {
+                case DbServerType.MsSql:
+                    schemaReader = new SqlServerSchemaReader();
+                    break;
+                case DbServerType.MySql:
+                    schemaReader = new MySqlSchemaReader();
+                    break;
+                case DbServerType.Postgres:
+                    schemaReader = new PostgreSqlSchemaReader();
+                    break;
+                default: break;
+            }
+
+            return schemaReader;
+        }
+    }
+}
