f2dd4cc5550567b0c631174e289e29216094ae86
Tran Duy Quang <duyquang19981@gmail.com>
2021-01-14 20:08:20 +0700

    MySqlSchema

---
+++ POCO/Enum/DbServerType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCO.Enum
+{
+    /// <summary>
+    /// This class represents DbServerType class.
+    /// </summary>
+    public enum DbServerType
+    {
+        MsSql,
+        MySql,
+        Postgres
+    }
+}
---
+++ POCO/Readers/SchemaReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCO.Readers
+{
+    using Models;
+
+    /// <summary>
+    /// This abstract class for schema reading.
+    /// </summary>
+    public abstract class SchemaReader : IDisposable
+    {
+        /// <summary>
+        /// Reads the Schema returning all tables in the databse.
+        /// </summary>
+        public abstract Tables ReadSchema(string connectionString);
+
+        /// <summary>
+        /// Disposes of objects
+        /// </summary>
+        public abstract void Dispose();
+        public Tables LoadAllTable()
+        {
+            return null;
+        }
+        //public Table LoadColumn(Table table)
+        //{
+        //    return null;
+        //}
+        //public List<Key> GetOuterKeys(Table tbl)
+        //{
+        //    return null;
+        //}
+        //public List<Key> GetInnerKeys(Table tbl)
+        //{
+        //    return null;
+        //}
+    }
+}
