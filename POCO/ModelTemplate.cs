3eedcf71a12679df43c2a33f08440e6cbc0f3e9e
Nguyen Hoang Quyen <hoangquyen7901@gmail.com>
2021-01-14 20:10:09 +0700

    #T4 C#

---
+++ POCO/ModelTemplate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCO.Templates
+{
+    using Models;
+
+    /// <summary>
+    /// This class represents ModelTemplate class.
+    /// This a partial class of the template class generated from ModelTemplate.tt.
+    /// Note that the namespace - PocoGen.Templates matches the template file namespace.
+    /// </summary>
+    partial class ModelTemplate
+    {
+        public string Namespace { get; set; }
+        public bool IncludeRelationships { get; set; }
+        public Table Table { get; set; }
+        public Tables Tables { get; set; }
+    }
+}
