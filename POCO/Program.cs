78cde860a71095f625db14b55eb031507b1e8c11
Đức Thắng <thangvu8948@gmail.com>
2021-01-16 02:33:03 +0700

    + fix read args from command line

--- POCO/Program.cs
+++ POCO/Program.cs
@@ -12,10 +12,10 @@ namespace POCO
         static void Main(string[] args)
         {
             //string[] agruments = { "-dbtype:mysql", "-conn_string:server=127.0.0.1;port=3306;user id=root; database=hotel", "-include_relationships:true", "-namespace:POCO", "-models_location:E:\\OneDrive - VNU-HCMUS\\FIT-HCMUS\\FIT-NAM4\\3.MẪU THIẾT KẾ HĐT\\DoAn\\ORM\\POCO\\HOTEL\\" };
-            string[] agruments = { "-dbtype:mssql", "-conn_string:Data Source =.; Initial Catalog = QLKHACHSAN; Persist Security Info = True; User ID = sa; Password = ", "-include_relationships:true", "-namespace:POCO", "-models_location:E:\\OneDrive - VNU-HCMUS\\FIT-HCMUS\\FIT-NAM4\\3.MẪU THIẾT KẾ HĐT\\DoAn\\ORM\\POCO\\QLKHACHSAN\\" };
+            //string[] agruments = { "-dbtype:mssql", "-conn_string:Data Source =.; Initial Catalog = QLKHACHSAN; Persist Security Info = True; User ID = sa; Password = ", "-include_relationships:true", "-namespace:POCO", "-models_location:E:\\OneDrive - VNU-HCMUS\\FIT-HCMUS\\FIT-NAM4\\3.MẪU THIẾT KẾ HĐT\\DoAn\\ORM\\POCO\\QLKHACHSAN\\" };
             //string[] agruments = { "" };
             var readerParameters = new ReaderParameters();
-            var result = readerParameters.ParseArguments(agruments);//result là kết quả parse các tham số,  thành công sẽ lưu các tham số vào readerParameters
+            var result = readerParameters.ParseArguments(args);//result là kết quả parse các tham số,  thành công sẽ lưu các tham số vào readerParameters
 
             switch (result)
             {
